ab0350e3cecb939fd7d83740788f916b3b00027e
ItsLewis-DE <thanhphong1110n@gmail.com>
2026-06-09 02:52:19 +0200

    ignore files

--- src/functions/transform/handler.py
+++ src/functions/transform/handler.py
@@ -9,9 +9,10 @@ BUCKET_RAW = os.getenv('BUCKET_RAW','raw-bucket')
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 def lambda_handler(event,context):
+    logger = logging.getLogger(__name__)
     s3_record = event['Records'][0]
     event_name = s3_record['eventName']
-    print(f"Đang xử lí: {event}")
+    logger.info(f"Đang xử lí: {event}")
 
     file_key = urlib.parse.unquote_plus(s3_record['s3']['object']['key'])
     file_name = Path(file_key).stem
@@ -19,7 +20,8 @@ def lambda_handler(event,context):
         try:
             df = wr.s3.read_csv(path=f's3://{BUCKET_RAW}/{file_name}.csv')
         except Exception as e:
-            logger.info(f"There is an error: {e}")
+            logger.error(f"Có lỗi xãy ra: {e}")
+            raise
         df['timestamp'] =pd.to_datetime(df['timestamp'])
         df['year'] = df['timestamp'].dt.year
         df['month'] = df['timestamp'].dt.month
@@ -29,3 +31,32 @@ def lambda_handler(event,context):
         df = df.drop_duplicates()
 
         df['interaction_type'] = df['interaction_type'].str.lower().str.strip()
+
+    elif file_name == 'products':
+        try:
+            df = wr.s3.read_csv(ppath=f's3://{BUCKET_RAW}/{file_name}.csv')
+        except Exception as e:
+            logger.error(f'Có lỗi xãy ra: {e}')
+            raise
+        df['cateogry'] = df['category'].str.title()
+        df['subcategory'] = df['subcategory'].str.title()
+        df['brand'] = df['brand'].str.title()
+        df['product_description'] = df['product_description'].replace('\n','').strim()
+
+        df['price'] = pd.to_numeric(df['price'],errors='coerce')
+        df['rating_avg'] = pd.to_numeric(df['rating_avg']).fillna(0)
+        df['review_count'] = pd.to_numeric(df['review_count'])
+        df['stock_quantity'] = pd.to_numeric(df['stock_quantity'])
+
+        #enrich data
+        conditions = [
+            (df['stock_quantity'] ==0),
+            (df['stock_quantity']<10),
+            (df['stock_quantity'] >=10)
+        ]
+        choices = ['Out of Stock','Low Stock','In stock']
+        df['stock_status'] = np.select(conditions,choices,default='Không xác định')
+        df['date_added'] = pd.to_datetime(df['date_added'])
+        df['month'] = df['date_added'].dt.month
+        df['year'] = df['date_added'].dt.year 
+        
